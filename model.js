ea8be6f72f11fbc1f5cc04bf34801bcbb18c0289
Elida <blubber3210@gmail.com>
2025-10-16 14:05:27 +0200

    endringer

--- model.js
+++ model.js
@@ -1,63 +1,93 @@
 const model = {
     app: {
+        currentPage: 'loginPage',
+
         page: [
             'loginPage',
             'mainFeed',
             'addPlaces',
             'placePage',
+            'registerUser',
         ]
     },
 
     viewState: {
+        registerUser: {
+            username: '',
+            password: '',
+            repeatPassword: '',
+        },
+
+        mainFeed: {
+            search: '',
+        },
+
         login: {
             userName: '',
             password: '',
-            loggedIn: false,
-            isSpooky: true, // finnes en måte å gjøre det her på cleanere (spooky er anonym)
         },
 
         addPlace: {
             id: null,
+            userID: null,
             title: '',
             address: '',
             city: '',
-            category: [],
+            categoryID: [],
             image: '',
             rateCoffee: null,
             rateFood: null,
             description: '',
-            submit: false,
         },
     },
 
     data: {
-        category: ['cafe', 'bakeri', 'restaurant'],
+        users: [
+            {
+                id: 0,
+                userName: 'Spooky',
+                password: '',
+                placesAdded: [0, 1],
+            },
+        ],
+
+        category: [
+            {
+                id: 0, name: 'cafe',
+            },
+            {
+                id: 1, name: 'bakeri',
+            },
+            {
+                id: 2, name: 'restaurant',
+            },
+        ],
 
         placesList: [
             {
                 id: 0,
+                userID: 0,
                 title: 'GLH Glutenfrihuset Tøyen',
                 address: 'Ålebergveien 50A',
                 city: 'Oslo',
-                category: ['cafe', 'bakeri'],
+                categoryID: [0, 1],
                 image: 'imgLink',
                 rateCoffee: 5,
                 rateFood: 5,
                 description: 'Velkommen til GLH Glutenfrihuset - ditt glutenfrie bakeri i OSlo',
-                submit: true,
             },
             {
                 id: 1,
+                userID: 0,
                 title: 'En restaurant',
                 address: 'En adresse - Veien10B',
                 city: 'Bergen',
-                category: ['restaurant', 'cafe'],
+                categoryID: [2, 0],
                 image: 'imgLink',
                 rateCoffee: 2,
                 rateFood: 4,
                 description: 'SKIKKELIG dårlig kaffe, men ok bra mat',
-                submit: true,
-            }
+            },
         ],
     },
 }
